44735ecf0cf5ad7eecaedbc44fc0e04c1f326c2f
Mansuri Villa <mansuriadnan2004@gmail.com>
2024-09-15 11:33:21 +0530

    Bug's Fixes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 import { mouseOver } from './modules/mouseover.js';
 import { offcanvsMenu } from './modules/offcanvasmenu.js';
+import { Slider } from './modules/slider.js';
 
 mouseOver();
 offcanvsMenu();
+Slider();
---
+++ js/modules/slider.js
@@ -0,0 +1,23 @@
+export function Slider() {
+    $(document).ready(function () {
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            autoWidth: true,
+            dots: false,
+            navText: ["<img src='../../assets/images/site_right_icon_fff.svg'>", "<img src='../../assets/images/site_right_icon_fff.svg'>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 2
+                }
+            }
+        })
+    });
+}
