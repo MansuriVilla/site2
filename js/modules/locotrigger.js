ae016d95a301afaaa0b9d7848600db9192e8dce3
Mansuri Villa <mansuriadnan2004@gmail.com>
2024-09-22 22:07:59 +0530

    Added

--- js/modules/locotrigger.js
+++ js/modules/locotrigger.js
@@ -1,28 +1,42 @@
 
-export function Locomotive(){
+export function Locomotive() {
 
-// --- SETUP START ---
-const locoScroll = new LocomotiveScroll({
+  // --- SETUP START ---
+  const locoScroll = new LocomotiveScroll({
     el: document.querySelector(".smooth-scroll"),
     smooth: true
   });
   // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
   locoScroll.on("scroll", ScrollTrigger.update);
-  
+
   // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
   ScrollTrigger.scrollerProxy(".smooth-scroll", {
     scrollTop(value) {
-      return arguments.length ? locoScroll.scrollTo(value, {duration: 0, disableLerp: true}) : locoScroll.scroll.instance.scroll.y;
+      return arguments.length ? locoScroll.scrollTo(value, { duration: 0, disableLerp: true }) : locoScroll.scroll.instance.scroll.y;
     }, // we don't have to define a scrollLeft because we're only scrolling vertically.
     getBoundingClientRect() {
-      return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
     },
     // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
     pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
   });
-  
+
   // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
   ScrollTrigger.defaults({ scroller: ".smooth-scroll" });
   // --- SETUP END ---
 }
+gsap.registerPlugin(ScrollTrigger)
+document.querySelectorAll('.right-box').forEach(function (box) {
+  gsap.to(box, {
+    opacity: 1,
+    duration: 1,
+    scrollTrigger: {
+      trigger: box,
+      start: "10% 50%",
+      end: "100% 50%",
+      markers: false,
+      scrub: true,
+    }
+  });
+});
