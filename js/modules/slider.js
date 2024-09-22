273bc5ce2b5a1517d3ca55792123910ad8197fcc
Mansuri Villa <mansuriadnan2004@gmail.com>
2024-09-22 19:48:38 +0530

    All files added

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -2,20 +2,41 @@ export function Slider() {
     $(document).ready(function () {
         $('.owl-carousel').owlCarousel({
             loop: true,
-            margin: 10,
+            margin: 30,
             nav: true,
-            autoWidth: true,
+            autoWidth: false,
             dots: false,
             navText: ["<img src='./assets/images/site_right_icon_fff.svg'>", "<img src='./assets/images/site_right_icon_fff.svg'>"],
             responsive: {
                 0: {
-                    items: 1
+                    items: 1.2
                 },
-                600: {
-                    items: 3
+                768: {
+                    items: 1.5
+                },
+                991: {
+                    items: 2.5
+                },
+                1920: {
+                    items: 3.5
+                }
+            }
+        })
+    });
+    $(document).ready(function () {
+        $('.client_slider').owlCarousel({
+            loop: true,
+            margin: 300,
+            nav: true,
+
+            dots: false,
+            navText: ["<img src='./assets/images/site_right_icon_fff.svg'>", "<img src='./assets/images/site_right_icon_fff.svg'>"],
+            responsive: {
+                991: {
+                    items: 2.5
                 },
                 1000: {
-                    items: 2
+                    items: 2.3
                 }
             }
         })
